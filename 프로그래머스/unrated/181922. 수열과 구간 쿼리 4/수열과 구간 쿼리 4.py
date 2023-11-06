20c4082269f49e69b3a2162dcbc4436670a25f00
hmmi <89797257+hmmiii@users.noreply.github.com>
2023-11-06 19:13:41 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 10.30 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -1,6 +1,8 @@
 def solution(arr, queries):
     for s, e, k in queries:
         for i in range(s,e+1):
-            arr[i] = arr[i] + 1 if not(i%k) else arr[i] + 0
-    
+            if k == 0:
+                arr[i] +=0
+            else:
+                arr[i] = arr[i] + 1 if not(i%k) else arr[i] + 0
     return arr
