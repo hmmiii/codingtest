19c8d40076ab70a76386e29e99c5f06fe4810f04
hmmi <89797257+hmmiii@users.noreply.github.com>
2023-11-06 19:38:44 +0900

    [level unrated] Title: 배열 만들기 2, Time: 9.95 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,14 +2,12 @@ def solution(l, r):
     answer = []
     for i in range(l,r+1):
         for j in str(i):
-            tmp = ''
             if j == '0' or j == '5':
                 continue
             else:
                 break
         else:
             answer.append(i)
-
     if not(answer):
         answer.append(-1)
     return answer
