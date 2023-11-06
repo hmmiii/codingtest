37b74211f1cc613b23941ea2f1cce9ee48edd324
hmmi <89797257+hmmiii@users.noreply.github.com>
2023-11-06 18:57:16 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 8.62 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -0,0 +1,15 @@
+def solution(arr, queries):
+    answer = []
+    
+    for s, e, k in queries:
+        tmp = []
+        for i in range(s, e + 1):
+            if arr[i] > k:
+                tmp.append(arr[i])
+        
+        if tmp:
+            answer.append(min(tmp) if tmp else -1)
+        else:
+            answer.append(-1)
+    
+    return answer
