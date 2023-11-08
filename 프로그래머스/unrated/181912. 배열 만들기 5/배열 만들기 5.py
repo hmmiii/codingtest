a05f8a006c9e6e4bec75b47693eb9d70b25589e0
hmmi <89797257+hmmiii@users.noreply.github.com>
2023-11-08 22:08:23 +0900

    [level unrated] Title: 배열 만들기 5, Time: 4.26 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
@@ -2,7 +2,7 @@ def solution(intStrs, k, s, l):
     answer = []
     for i in intStrs:
         n  = 0 
-        if len(i[s:s+l]) >= l:
+        if len(i[s:s+l]):
             n = int(''.join([x for x in i[s:s+l]]))
         if n > k:
             answer.append(n)
